d6f0ff277870c83d9352a02c89d18b10f827da85
iversenespen-afk <iversen.espen@gmail.com>
2026-08-02 21:46:26 +0200

    Bug returning artist

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -21,7 +21,7 @@ const { data: matches, error } = await supabase
   )
 `)
 .order("id");
-  console.dir(matches, { depth: null });
+  console.dir({ matches, error }, { depth: null });
 
   if (error) {
     return (
