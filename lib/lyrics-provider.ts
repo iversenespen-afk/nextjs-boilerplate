fb1f88ecae228320552ccbafd40df962296794fd
iversenespen-afk <iversen.espen@gmail.com>
2026-08-06 00:03:39 +0200

    Add lyrics provider abstraction

---
+++ lib/lyrics-provider.ts
@@ -0,0 +1,20 @@
+export type LyricsRequest = {
+  spotifyId: string;
+  artist: string;
+  title: string;
+};
+
+export type LyricsResult = {
+  lyrics: string;
+  provider: string;
+};
+
+export async function getLyrics(
+  request: LyricsRequest,
+): Promise<LyricsResult> {
+  void request;
+
+  throw new Error(
+    "Ingen lyrics-leverandør er konfigurert ennå.",
+  );
+}
