babc26c92c6fd91a77d719dd38c59d6251b88af0
iversenespen-afk <iversen.espen@gmail.com>
2026-08-11 01:23:30 +0200

    Update lyrics-provider ny mock Vinsjan

--- lib/lyrics-provider.ts
+++ lib/lyrics-provider.ts
@@ -19,9 +19,9 @@ I was listening to Tool on the radio last night.
     `.trim(),
 
     // D.D.E. – Vinsjan på kaia
-    "3XaBeTuIQEhTcELhfNch7Q": `
-I heard Dr. Dre, Eminem, Tommy Lee and Tom Green on the radio.
-    `.trim(),
+"3XaBeTuIQEhTcELhfNch7Q": `
+I was listening to Eminem, Tool and Beatles on the radio.
+`.trim(),
   };
 
   const lyrics = mockLyricsBySpotifyId[request.spotifyId];
