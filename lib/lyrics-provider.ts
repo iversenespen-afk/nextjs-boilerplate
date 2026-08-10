f05fd9f3c53c6ac8286ea5544606bd88934e0aca
iversenespen-afk <iversen.espen@gmail.com>
2026-08-11 01:19:04 +0200

    Skrivefeil Lyrics provider

--- lib/lyrics-provider.ts
+++ lib/lyrics-provider.ts
@@ -19,7 +19,7 @@ I was listening to Tool on the radio last night.
     `.trim(),
 
     // D.D.E. – Vinsjan på kaia
-    "3XaBeTulQEhTcELhfNch7Q": `
+    "3XaBeTuIQEhTcELhfNch7Q": `
 I heard Dr. Dre, Eminem, Tommy Lee and Tom Green on the radio.
     `.trim(),
   };
