492ef475200b80a4a3135d0b9be7809967560ae5
iversenespen-afk <iversen.espen@gmail.com>
2026-08-11 17:43:05 +0200

    Create theme-concept-classes.ts

---
+++ lib/theme-concept-classes.ts
@@ -0,0 +1,9 @@
+export const THEME_CONCEPT_CLASSES: Record<string, string[]> = {
+  artists: ["person", "band"],
+  body_parts: ["body_part"],
+  colors: ["color"],
+  instruments: ["instrument"],
+  planets: ["planet", "fictional_planet"],
+  us_states: ["place"],
+  trees: ["tree"],
+};
