97417a5685c7641be9ee23d8226dc907f1d4cd3e
iversenespen-afk <iversen.espen@gmail.com>
2026-08-16 14:01:24 +0200

    Delete lib/theme-concept-classes.ts

--- lib/theme-concept-classes.ts
+++
@@ -1,9 +0,0 @@
-export const THEME_CONCEPT_CLASSES: Record<string, string[]> = {
-  artists: ["person", "band"],
-  body_parts: ["body_part"],
-  colors: ["color"],
-  instruments: ["instrument"],
-  planets: ["planet", "fictional_planet"],
-  us_states: ["place"],
-  trees: ["tree"],
-};
